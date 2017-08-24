3ddefda4c53ef557bfe4e47c967a22369df8c407
katiedeangelis <katie.e.deangelis@gmail.com>
2017-08-24 09:32:19 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,39 @@
+// when grow button is clicked...
+document.getElementById("button1").addEventListener("click", function(){
+
+	//increase the size of the box.
+    document.getElementById("box").style.height = "250px";
+
+    document.getElementById("box").style.width = "250px";
+
+});
+
+// when blye button is clicked...
+document.getElementById("button2").addEventListener("click", function(){
+
+   	//change box color to blue.    	
+   	document.getElementById("box").style.backgroundColor = "Blue";
+
+});
+
+// when fade button is clicked...
+document.getElementById("button3").addEventListener("click", function(){
+
+   	//make the box fade.
+   	document.getElementById("box").style.opacity = "0.5";
+
+});
+
+// when resent button is clicked...
+document.getElementById("button4").addEventListener("click", function(){
+
+	//make the box orange and regular size.
+    document.getElementById("box").style.height = "150px";
+
+    document.getElementById("box").style.width = "150px";
+
+    document.getElementById("box").style.backgroundColor = "orange";
+
+    document.getElementById("box").style.opacity = "1.0";
+
+});
